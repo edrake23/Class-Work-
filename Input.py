d449adcffbf05bc536c07e04f4529eea70002364
edrake23 <edrake23@gmail.com>
2017-01-12 21:57:13 -0500

    Project 3 Struggle

---
+++ Input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 18 15:20:00 2016
+
+@author: ericdrake
+"""
+
+hrs = raw_input("Enter Hours:")
+h = float(hrs)
+
+rate = raw_input("Enter Rate:")
+r = float(rate)
+
+pay = h*r
